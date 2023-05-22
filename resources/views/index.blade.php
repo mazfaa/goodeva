5c7417664f1ea01ef06b529f7af0a969a196558e
Muhammad Azfa Asykarulloh <kangazfa@gmail.com>
2023-05-22 07:47:10 +0700

    fixing dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,95 +19,77 @@
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-12">
-      <div class="card">
-        <div class="card-header">
-          Employees Attendance
+  <section class="row">
+    <div class="col-12 col-lg-12">
+        <div class="row">
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                <div class="stats-icon purple mb-2">
+                                    <i class="iconly-boldShow"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Employees</h6>
+                                <h6 class="font-extrabold mb-0">112.000</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                <div class="stats-icon blue mb-2">
+                                    <i class="iconly-boldProfile"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Offices</h6>
+                                <h6 class="font-extrabold mb-0">183.000</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                <div class="stats-icon green mb-2">
+                                    <i class="iconly-boldAdd-User"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Job Status</h6>
+                                <h6 class="font-extrabold mb-0">80.000</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-4 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                <div class="stats-icon red mb-2">
+                                    <i class="iconly-boldBookmark"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Professions</h6>
+                                <h6 class="font-extrabold mb-0">112</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table-hover table-bordered" id="employee_table">
-              <thead>
-                <tr>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      <input type="checkbox" name="check-all" id="check-all"> <span class="ms-3">Employee</span> <span class="ms-1">Name</span>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Employee <span class="ms-1">ID</span>
-                    </span>
-                  </th>
-                  <th>Date</th>
-                  <th>Shift</th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Attendance <span class="ms-1">Status</span>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Clock <span class="ms-1">In</span>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Clock <span class="ms-1">Out</span>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Absent <span class="ms-1">Reason</span> <i class="ms-2 fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-title="Reason for why not present"></i>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Presence <span class="ms-1">Location</span>
-                    </span>
-                  </th>
-                  <th>Manager</th>
-                  <th>Division</th>
-                  <th>Profession</th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Job <span class="ms-1">Level</span>
-                    </span>
-                  </th>
-                  <th>
-                    <span class="d-flex align-items-center">
-                      Employment <span class="ms-2">Status</span>
-                    </span>
-                  </th>
-                  <th>Branch</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+      </section>
 </x-app>
