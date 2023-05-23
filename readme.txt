72ebbc6678142616de879a0067dac2e6eaf39734
Muhammad Azfa Asykarulloh <kangazfa@gmail.com>
2023-05-23 20:03:18 +0700

    test

--- readme.txt
+++
@@ -1,8 +0,0 @@
-owner :
-	email : goodeva@goodeva.co.id
-	password : admin
-
-import file regencies.csv pada tabel cities
-import file province.csv pada tabel province
-
-Daftarkan karyawan -> logout -> login menggunakan data karyawan tadi -> absensi
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Goodeva Technology</title>
+    <title>Tes Login | Goodeva Technology</title>
     <link rel="stylesheet" href="assets/css/main/app.css">
     <link rel="stylesheet" href="assets/css/main/app-dark.css">
     <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
